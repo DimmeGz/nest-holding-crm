d2761494fabe7389af546ee0f4dcd2e87a43c9e3
dimmex <dima.sergienko@gmail.com>
2024-09-05 11:19:03 +0300

    chore: add config validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { VALIDATION_SCHEMA } from './config';
+
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ validationSchema: VALIDATION_SCHEMA })],
   controllers: [AppController],
   providers: [AppService],
 })
